4253d70f8750e938ba9ddbb37c6abc9f1c4cfa34
Ervin <3lue06@gmail.com>
2022-09-19 16:58:54 +0100

    Initial commit

---
+++ A100/Basics.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A100
+{
+    internal class Basics
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input your first number");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Input your second number");
+            int num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("The sum is: " + (num1 + num2));
+            Console.WriteLine("The product is: " + num1 * num2);
+            Console.WriteLine("The sum, squared, is: " + Math.Pow(num1 + num2, 2));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ A100/SlabCalc.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A100
+{
+    internal class SlabCalc
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input number of slabs across");
+            int slabsAcross = int.Parse(Console.ReadLine());
+            Console.WriteLine("Input number of slabs deep");
+            int slabsDeep = int.Parse(Console.ReadLine());
+            Console.WriteLine("Input price of a slab");
+            Console.Write("£");
+            double slabPrice = double.Parse(Console.ReadLine());
+            double totalPrice = slabsAcross * slabsDeep * slabPrice;
+            string strtotalPrice = "£" + String.Format("{0:0.00}", totalPrice);
+            Console.WriteLine("Your total price: " + strtotalPrice);
+            Console.ReadKey();
+        }
+    }
+}
