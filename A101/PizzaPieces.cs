aa294cd7c0d6c13199eb36cf93e331f5dd4ae51a
Ervin <37591724+3leh@users.noreply.github.com>
2022-10-02 17:35:56 +0100

    Added A101

---
+++ A101/PizzaPieces.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A101
+{
+    internal class PizzaPieces
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input diameter (cm):");
+            int diameter = int.Parse(Console.ReadLine());
+            Console.WriteLine("Input number of slices:");
+            int slices = int.Parse(Console.ReadLine());
+            Console.WriteLine("Input number of slices eaten:");
+            int eaten = int.Parse(Console.ReadLine());
+            const int thickness = 2;
+            double volume = Math.PI * diameter / 2 * eaten / slices * thickness;
+            Console.WriteLine(volume + "cm^3 of pizza eaten");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ A101/SelectionBasics2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A101
+{
+    internal class SelectionBasics2
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input a number:");
+            int num = int.Parse(Console.ReadLine());
+            if (num < 5000 && num > 1000)
+            {
+                Console.WriteLine("Correct!");
+            }
+            else if (num < 1000)
+            {
+                Console.WriteLine("Too small");
+            }
+            else //(greater than 5000)
+            {
+                Console.WriteLine("Too big");
+            }
+            Console.ReadKey();
+        }
+    }
+}
