1bf417f9646b9bde40a5a65997a2bb07ffc3a891
Ervin <37591724+3leh@users.noreply.github.com>
2023-03-12 20:31:19 +0000

    Add A121

---
+++ A121/PizzaOrder.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A121
+{
+    internal class PizzaOrder
+    {
+        private string address;
+        private int diameter;
+        private List<string> toppings;
+        public PizzaOrder(string addressToDeliver, int size, List<string> listOfToppings)
+        {
+            address = addressToDeliver;
+            diameter = size;
+            toppings = listOfToppings;
+        }
+        public int getPrice()
+        {
+            double price = ((Math.Pow((diameter / 2), 2) * Math.PI) / 10) + toppings.Count + 2;
+            return (int)Math.Round(price);
+        }
+        public void getSummary()
+        {
+            Console.WriteLine($"Address: {address}");
+            Console.WriteLine($"Size: {diameter} inches");
+            Console.WriteLine($"Toppings ({toppings.Count} total): ");
+            foreach (string topping in toppings)
+            {
+                Console.WriteLine($"•{topping}"); 
+            }
+            Console.WriteLine($"Price: £{getPrice()}.00");
+        }
+    }
+}
---
+++ A121/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A121
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            List<string> tops = new List<string>();
+
+            Console.WriteLine("Thank you for ordering from 'Computers Ordered My Pizzas, Secret Company Inc.', your order is being calculated");
+            Console.WriteLine("...");
+            System.Threading.Thread.Sleep(1000);
+
+            tops.Add("ham");
+            tops.Add("pineapple");
+            tops.Add("anchovies");
+            tops.Add("olives");
+            tops.Add("a cuddly toy");
+
+
+            PizzaOrder newOrder = new PizzaOrder("***REMOVED***, ***REMOVED***", 12, tops);
+
+            Console.WriteLine("The pizza you have ordered comes to £" + newOrder.getPrice());
+            System.Threading.Thread.Sleep(1000);
+            Console.WriteLine("Please confirm that this is correct:");
+            Console.WriteLine("--------------------");
+            newOrder.getSummary();
+            Console.WriteLine("--------------------");
+            Console.ReadKey();
+        }
+    }
+}
