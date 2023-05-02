ba4ccf67ca55e08fb5cac72943b7a63c434c993d
Ervin <37591724+3leh@users.noreply.github.com>
2023-05-02 14:14:04 +0100

    Add A126

---
+++ A126/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A126
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Enter a number for double factorial: ");
+			int input = int.Parse(Console.ReadLine());
+			int iterativeAns = Iterative(input);
+			Console.WriteLine($"Iterative: \n{WorkingOut} = {iterativeAns}");
+			WorkingOut = null;
+			int recursiveAns = Recursive(input);
+			Console.WriteLine($"Recursive: \n{WorkingOut} = {Recursive(input)}");
+			Console.ReadKey();
+		}
+		static string WorkingOut;
+		static int Iterative(int input)
+		{
+			int result = 1;
+			for (int i = input; i > 0; i -= 2)
+			{
+				result *= i;
+				WorkingOut += String.IsNullOrEmpty(WorkingOut) ? i.ToString() : $" × {i}";
+			}
+			if(WorkingOut[WorkingOut.Length - 1] != '1') { WorkingOut += " × 1"; }
+			return result;
+		}
+		static int Recursive(int input)
+		{
+			if (input == 1 || input == 0)
+			{
+				WorkingOut += " × 1";
+				return 1;
+			}
+			else
+			{
+				WorkingOut += String.IsNullOrEmpty(WorkingOut) ? input.ToString() : $" × {input}";
+				return (input * Recursive(input - 2));
+			}
+		}
+	}
+}
