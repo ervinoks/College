c1408cc7efbd41c6828a575bd1bd617a32c43bc4
Ervin <37591724+3leh@users.noreply.github.com>
2023-05-24 14:59:01 +0100

    Sort projects into folders

---
+++ Lessons/L100/FirstProgram.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L100
+{
+    internal class FirstProgram
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int numSubjects = int.Parse(Console.ReadLine());
+            Console.WriteLine(name, "takes", numSubjects, "subjects");
+            Console.ReadKey();
+        }
+    }
+}
