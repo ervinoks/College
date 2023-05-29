4d0d94054db8efa6a9448d38620edb0b714446d7
Ervin <37591724+3leh@users.noreply.github.com>
2023-05-29 02:16:30 +0100

    Add A130
    This is the same program as: https://github.com/3leh/College/blob/main/Assessments/CAP1/Program4.cs
    However this is redone for revision, therefore code has improved.

---
+++ Assignments/A130/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A130
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.Write("Input the text to compress: ");
+			string text = Console.ReadLine();
+			StringBuilder compressedText = new StringBuilder();
+			char currentChar = text[0];
+			int charCount = 1;
+			for (int i = 1; i < text.Length; i++)
+			{
+				if (currentChar == text[i])
+				{
+					charCount++;
+				}
+				else
+				{
+					compressedText.Append($"{currentChar} {charCount} ");
+					currentChar = text[i];
+					charCount = 1;
+				}
+			}
+			compressedText.Append($"{currentChar} {charCount}");
+			Console.WriteLine(compressedText.ToString());
+			Console.ReadKey();
+		}
+	}
+}
