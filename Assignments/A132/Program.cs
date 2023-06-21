5e7e7e13c12d2569ccaf224fb91af801613cadff
Ervin <37591724+3leh@users.noreply.github.com>
2023-06-21 19:51:29 +0100

    Add A132

---
+++ Assignments/A132/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A132
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Enter an ISBN number:");
+			string isbn = Console.ReadLine();
+			if (IsValid(isbn)) Console.WriteLine("The ISBN number is valid.");
+			else Console.WriteLine("Invalid ISBN number.");
+			Console.ReadKey();
+		}
+		static bool IsValid(string isbn)
+		{
+			int sum = 0;
+			for (int i = 0; i < isbn.Length - 1; i++)
+			{
+				char c = isbn[i];
+				if (char.IsDigit(c))
+				{
+					int d = int.Parse(c.ToString());
+					sum += i % 2 == 0 ? d : d * 3;
+				}
+				else return false;
+			}
+			int checkDigit = (10 - sum % 10) % 10;
+			char lastChar = isbn[isbn.Length - 1];
+			if (!char.IsDigit(lastChar)) 
+				return false;
+			return checkDigit == int.Parse(lastChar.ToString());
+		}
+	}
+}
+
