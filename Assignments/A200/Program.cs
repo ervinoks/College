3765abf24eb6d8ea2b7a7f47b6c6acd4beb97f7d
Ervin <37591724+ervinoks@users.noreply.github.com>
2023-09-25 18:52:56 +0100

    Add 2nd year Assignments

---
+++ Assignments/A200/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A200
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			List<string> words = new List<string> { "ABodE", "abc", "xYz", "xBkd" };
+			Console.WriteLine($"{string.Join(", ", words)} : {string.Join(", ", AlphabetPositionCount(words))}");
+			Console.ReadKey();
+		}
+
+		static List<int> AlphabetPositionCount(List<string> words)
+		{
+			List<int> counts = new List<int>();
+			
+			foreach (string word in words)
+			{	
+				int count = 0;
+				for (int i = 0; i < word.Length; i++)
+				{
+					char letter = word[i];
+					letter = char.ToLower(letter);
+					// calc the expected position in the alphabet (0-based)
+					int expectedPosition = letter - 'a';
+					// check if the letter's position matches its position in the alphabet
+					if (expectedPosition == i)
+					{
+						count++;
+					}
+				}
+				counts.Add(count);
+			}
+			return counts;
+		}
+	}
+}
---
+++ Assignments/A201/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A201
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.Write("Enter start map: ");
+			string map = Console.ReadLine();
+			(string map, int total, int infected) world = Pandemic(map.ToCharArray());
+			Console.WriteLine($"End map: {world.map}");
+			Console.WriteLine($"Total: {world.total}");
+			Console.WriteLine($"Infected: {world.infected}");
+			Console.WriteLine($"Percentage: {Math.Round((float)world.infected / (float)world.total * 100)}%");
+			Console.ReadKey();
+		}
+		private static (string map, int total, int infected) Pandemic(char[] map)
+		{
+			bool infect = true;
+			int oceanCount = 0; 
+			(int prev, int current) oceanIndex = (-1, -1);
+			for (int i = 0; i < map.Length; i++)
+			{
+				if (map[i] == 'X')
+				{
+					oceanCount++;
+					if (oceanCount > 0) oceanIndex.prev = oceanIndex.current;
+					oceanIndex.current = Array.IndexOf(map, 'X', oceanIndex.prev + 1);
+					if (infect == true) spread();
+					infect = false;
+				}
+				else if (map[i] == '1')
+				{
+					infect = true;
+				}
+				if (infect == true && i == 0) spread();
+			}
+			void spread()
+			{
+				for (int j = oceanIndex.prev + 1; j < oceanIndex.current; j++)
+				{
+					map[j] = '1';
+				}
+			}
+			int infected = map.Count(c => c == '1');
+			return (new string(map), map.Count(c => c == '0') + infected, infected);
+		}
+	}
+}
