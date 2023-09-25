0ff949d79e69251841bb477bede989c6676e65f5
Ervin <37591724+ervinoks@users.noreply.github.com>
2023-09-25 18:48:46 +0100

    Add A135

---
+++ Assignments/A135/CensoredStrings.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A135
+{
+	internal class CensoredStrings
+	{
+		static void Main(string[] args)
+		{
+			string _, __; // saves me rewriting
+			Console.WriteLine($"uncensor({_ = "*h*s *s v*ry *tr*ng*"}, {__ = "Tiiesae"}): {uncensor(_, __)}");
+			Console.WriteLine($"uncensor({_ = "A**Z*N*"}, {__ = "MAIG"}): {uncensor(_, __)}");
+			Console.WriteLine($"uncensor({_ = "xyz"}, {__ = ""}): {uncensor(_, __)}");
+			Console.ReadKey();
+		}
+		static string uncensor(string text, string censored)
+		{
+			string output = string.Empty;
+			StringReader censR = new StringReader(censored);
+			using (StringReader reader = new StringReader(text))
+			{
+				while (reader.Peek() != -1)
+				{
+					char nextChar = (char)reader.Read();
+					if (nextChar == '*') output += (char)censR.Read();
+					else output += nextChar;
+				} 
+			}
+			return output;
+		}
+	}
+}
---
+++ Assignments/A135/DisariumNumber.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A135
+{
+	internal class DisariumNumber
+	{
+		static void Main(string[] args)
+		{
+			int _; // saves me rewriting
+			Console.WriteLine($"{_ = 175}: {Disarium(_)}");
+			Console.WriteLine($"{_ = 375}: {Disarium(_)}");
+			Console.WriteLine($"{_ = 518}: {Disarium(_)}");
+			Console.ReadKey();
+		}
+		static bool Disarium(int num)
+		{
+			int _tot = default;
+			for (int i = 0; i < num.ToString().Length; i++)
+				_tot += (int)Math.Pow(int.Parse(num.ToString()[i].ToString()), i + 1);
+			return _tot == num;
+		}
+	}
+}
