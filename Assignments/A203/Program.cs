4e3030b28b4303450f09adf1321be6c20cd36bf9
Ervin <37591724+ervinoks@users.noreply.github.com>
2023-10-10 15:16:12 +0100

    Add A203

---
+++ Assignments/A203/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A203
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] A = { 2, 6, 14, 24, 39, 57, 63, 88 };
+			int searchNum = int.Parse(Console.ReadLine());
+			Console.WriteLine(BinarySearch(A, searchNum));
+			Console.ReadKey();
+		}
+		static string BinarySearch(int[] array, int num)
+		{
+			int len = array.Length,
+				mid = (len / 2) - 1, 
+				lb = 0, 
+				ub= len - 1;
+			bool solved = false;
+			int count = 0;
+			while (solved == false)
+			{
+				mid = (ub + lb) / 2;
+				if (count >= Math.Log(array.Length))
+					break;
+				int midVal = array[mid];
+				if (midVal < num)
+				{
+					lb = mid + 1;
+					count++;
+				}
+				else if (midVal > num)
+				{
+					ub = mid - 1;
+					count++;
+				}
+				else
+					solved = true;
+			}
+			return solved ? $"Found {num} at index {mid}" : $"Could not find {num} in array";
+		}
+	}
+}
