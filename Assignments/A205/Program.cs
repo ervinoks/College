75390e124f2c2ae5860293e2bf0456ac63ef2c5d
Ervin <37591724+ervinoks@users.noreply.github.com>
2023-10-31 00:03:59 +0000

    Add A205

---
+++ Assignments/A205/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A205
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine(IsHappy(86) + "\n");
+			Console.WriteLine(IsHappy(55));
+			Console.ReadKey();
+		}
+		static int[] UnhappyNums = { 4, 16, 20, 37, 42, 58, 89 };
+		static bool IsHappy(int n)
+		{
+			int sum = 0;
+			foreach (var digit in n.ToString())
+			{
+				Console.Write($"{digit}² + ");
+				sum += (int)Math.Pow(int.Parse(digit.ToString()), 2);
+			}
+			Console.CursorLeft -= 3;
+			Console.WriteLine($" = {sum}");
+			if (UnhappyNums.Contains(sum))
+				return false;
+			else if (sum == 1)
+				return true;
+			else
+				return IsHappy(sum);
+		}
+	}
+}
