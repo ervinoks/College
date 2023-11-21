68e8c54f7dbed1ee82cc7b8e4878db8114cef5a1
Ervin <37591724+ervinoks@users.noreply.github.com>
2023-11-21 15:11:54 +0000

    Add A208

---
+++ Assignments/A208/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A208
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			string student, dish;
+			Console.WriteLine($"Can {student = "cho chang"} bring { dish = "chocolate pudding"}? {feast(student, dish)}");
+			Console.WriteLine($"Can {student = "ron weasley"} bring {dish = "raspberry trifle"}? {feast(student, dish)}");
+			Console.ReadKey();
+		}
+		static bool feast(string student, string dish)
+		{
+			if (student[0] == dish[0])
+			{
+				if (student[student.Length - 1] == dish[dish.Length - 1])
+				{
+					return true;
+				}
+			}
+			return false;
+		}
+	}
+}
