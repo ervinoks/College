99a19abd5231b079b3ef2221c050cd87327098a3
Ervin <37591724+ervinoks@users.noreply.github.com>
2024-01-19 08:39:44 +0000

    Add A215

---
+++ Assignments/A215/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A215
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int DADoorperson(string word) => (word.IndexOf(word.First(c => c == (word[word.IndexOf(c) + 1]))) + 1) * 3;
+			Console.WriteLine($"potter: {DADoorperson("potter")}\ntarantallegra: {DADoorperson("tarantallegra")}");
+			Console.ReadKey();
+		}
+	}
+}
