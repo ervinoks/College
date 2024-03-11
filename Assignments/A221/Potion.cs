1f6c2dc7ec73730053501f448202ef0d81128d1e
Ervin <37591724+ervinoks@users.noreply.github.com>
2024-03-11 12:52:46 +0000

    Add A221

---
+++ Assignments/A221/Potion.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A221
+{
+	internal class Potion
+	{
+		protected int[] Colour { get; set; }
+		protected int Volume { get; set; }
+		public Potion(int Volume, params int[] colours)
+		{
+			if (colours.Length != 3) throw new ArgumentException("Potion must have 3 colour values.");
+			Colour = colours;
+			this.Volume = Volume;
+		}
+		public int[] getColour => Colour;
+		public int getVolume => Volume;
+		public Potion Mix(Potion secondPotion)
+		{
+			List<int> mixedColour = new List<int>();
+			for (int i = 0; i < Colour.Length; i++) 
+			{
+				mixedColour.Add((Colour[i] * Volume + secondPotion.Colour[i] * secondPotion.Volume) / (Volume + secondPotion.Volume));
+			}
+			return new Potion(Volume + secondPotion.Volume, mixedColour.ToArray());
+		}
+	}
+}
---
+++ Assignments/A221/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A221
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Potion Felix_Felicis = new Potion(10, 192, 192, 192);
+			Potion Polyjuice_Potion = new Potion(5, 0, 128, 0);
+			Potion Mixed_Potion = new Potion(0, 0, 0, 0);
+			Mixed_Potion = Felix_Felicis.Mix(Polyjuice_Potion);
+			Console.Write($"Felix Felicis: \n  Volume: {Felix_Felicis.getVolume}\n  Colour: ");
+			PrintColour(Felix_Felicis);
+			Console.Write($"\nPolyjuice Potion: \n  Volume: {Polyjuice_Potion.getVolume}\n  Colour: "); 
+			PrintColour(Polyjuice_Potion);
+			Console.Write($"\n\nMixed Potion: \n  Volume: {Mixed_Potion.getVolume}\n  Colour: ");
+			PrintColour(Mixed_Potion);
+			Console.ReadKey();
+			void PrintColour(Potion potion)
+			{
+				for (int i = 0; i < 3; i++)
+				{
+					int[] PotionColours = potion.getColour;
+					Console.ForegroundColor = ConsoleColor.Black;
+					ConsoleColor[] colours = { ConsoleColor.Red, ConsoleColor.Green, ConsoleColor.Blue };
+					Console.BackgroundColor = colours[i];
+					Console.Write(PotionColours[i]);
+					Console.ResetColor();
+					if (i == 0 || i == 1) Console.Write(", ");
+				}
+			}
+		}
+	}
+}
